7cc68ecb46743f2d1938d5155640c58cb20d53d6
David Klemme <david@berlinersoftwareschmiede.de>
2023-06-20 16:37:28 +0200

    batch calls for speed

--- app/match.py
+++ app/match.py
@@ -21,7 +21,7 @@ def persistData(data, connection, extras):
 
     # MATCH
     # TODO add rest of data to insert
-    extras.execute_batch(cursor,cursor.mogrify("""
+    extras.execute_batch(cursor,"""
         INSERT INTO 
             matches 
         (
@@ -39,8 +39,8 @@ def persistData(data, connection, extras):
         VALUES 
             (%s, %s, %s, %s, %s, %s, %s, %s, %s, (SELECT id from teams where name = %s limit 1))
         RETURNING id;
-        """, (
-            matches.get('event').get('name'), 
+        """, [
+            (matches.get('event').get('name'), 
             matches.get('event').get('match_number'), 
             matches.get('match_type'),
             matches['venue'], 
@@ -49,9 +49,9 @@ def persistData(data, connection, extras):
             matches.get('gender'),
             matches.get('overs'), 
             matches.get('team_type'), 
-            matches.get('outcome').get('winner') 
-        )
-    ),pageSize)
+            matches.get('outcome').get('winner') )
+        ]
+    ,pageSize)
     
     match_id = cursor.fetchone()
     connection.commit()
@@ -61,14 +61,14 @@ def persistData(data, connection, extras):
         if isinstance(team, str):
             extras.execute_batch(cursor,"""
                         INSERT INTO teams (name, matches_id, team_type) VALUES (%s, %s, %s);
-                    """, (team,match_id, matches.get('team_type')),pageSize)
+                    """, [(team,match_id, matches.get('team_type'))],pageSize)
             
             connection.commit()
             
     # OUTCOMES
     '''I miss Elvis :('''
     if matches.get('outcome').get('by') is None:
-        extras.execute_batch(cursor,cursor.mogrify(
+        extras.execute_batch(cursor,
             """INSERT INTO 
                 outcomes (
                     matches_id, 
@@ -87,17 +87,17 @@ def persistData(data, connection, extras):
                     (SELECT id from teams where name= %s)
                 )
             """,
-            (
+            [(
                 match_id, 
                 matches.get('outcome').get('bowl_out'), 
                 matches.get('outcome').get('eliminator'), 
                 matches.get('outcome').get('method'), 
                 matches.get('outcome').get('result'), 
                 matches.get('team')
-            )
-        ),pageSize)
+            )]
+        ,pageSize)
     else:
-        extras.execute_batch(cursor,cursor.mogrify(
+        extras.execute_batch(cursor,
             """INSERT INTO 
                 outcomes (
                     matches_id, 
@@ -118,11 +118,11 @@ def persistData(data, connection, extras):
                 %s,
                 (SELECT id from teams where name= %s),
                 %s,
-                %s ,
+                %s,
                 %s
                 )
             """,
-            (
+           [ (
                 match_id, 
                 matches.get('outcome').get('bowl_out'), 
                 matches.get('outcome').get('eliminator'), 
@@ -132,50 +132,56 @@ def persistData(data, connection, extras):
                 matches.get('outcome').get('by').get('runs'),
                 matches.get('outcome').get('by').get('innings'),
                 matches.get('outcome').get('by').get('wickets')
-            )
-        ),pageSize)
+            )]
+        ,pageSize)
 
     connection.commit()
     
     # PLAYER INFO
+    playerValue = []
     for team, players in matches['players'].items():
         for player in players:
-            extras.execute_batch(cursor,cursor.mogrify("""
+            playerValue.append((player, team))
+            extras.execute_batch(cursor,"""
                 INSERT INTO players (name, team_id)
                 SELECT %s,id FROM teams WHERE name = %s;
-            """, (player, team)),pageSize)
+            """, playerValue,pageSize)
     
     # INNINGS
+    inningsValue=[]
     for inning in innings:
         team_name = inning['team']
-    
-        extras.execute_batch(cursor,cursor.mogrify("""
+        inningsValue.append((team_name, match_id))
+    extras.execute_batch(cursor,"""
             INSERT INTO innings (team_id, matches_id)
             VALUES ((SELECT id FROM teams WHERE name = %s limit 1),%s)
             RETURNING id;
-        """, (team_name, match_id)),pageSize)
-        inning_id = cursor.fetchone()
-        connection.commit()
+        """, inningsValue,pageSize)
+    inning_id = cursor.fetchone()
+    connection.commit()
         
         
     # OVERS 
-        for over in inning['overs']:
-            extras.execute_batch(cursor,cursor.mogrify("""
+    overValues= []
+    for over in inning['overs']:
+            overValues.append((inning_id, over['over']))
+    extras.execute_batch(cursor,"""
                 INSERT INTO overs (inning_id, over_number)
                 VALUES (%s, %s)
                 RETURNING id;
-            """, (inning_id, over['over'])),pageSize)
+            """, overValues,pageSize)
             
-            over_id = cursor.fetchone()  
+    over_id = cursor.fetchone()  
             
     # DELIVERIES
-            for delivery in over['deliveries']:
+    deliveryValues= []
+    for delivery in over['deliveries']:
                 batter = delivery['batter']
                 bowler = delivery['bowler']
                 non_striker = delivery['non_striker']
                 runs = delivery['runs']
                 wickets = delivery.get('wickets', [])
-                extras = delivery.get('extras', {})
+                extrass = delivery.get('extras', {})
             #    cursor.execute_batch("""
             #         INSERT INTO deliveries (over_id, batter_id, bowler_id, non_striker_id, runs_batter, runs_extras)
             #         VALUES (
@@ -189,7 +195,8 @@ def persistData(data, connection, extras):
             #     """, (over_id, batter,over_id, bowler,over_id, non_striker,over_id, runsBatter, runsExtras)) 
 
     # Using LIMIT 1 is a 'little' dirty, but time is running out. SELECT needs to be joined to make sure that the correct match/team (international vs club) is correctly depicted.
-                extras.execute_batch(cursor,cursor.mogrify("""
+                deliveryValues.append((over_id, batter, bowler, non_striker, runs['batter'], runs['extras']))
+    extras.execute_batch(cursor,"""
                     INSERT INTO deliveries (over_id, batter_id, bowler_id, non_striker_id, runs_batter, runs_extras)
                     VALUES (
                         %s, 
@@ -199,20 +206,24 @@ def persistData(data, connection, extras):
                         %s, 
                         %s)
                     RETURNING id;
-                """, (over_id, batter, bowler, non_striker, runs['batter'], runs['extras'])),pageSize)
+                """, deliveryValues,pageSize)
                 
-                delivery_id = cursor.fetchone() 
+    delivery_id = cursor.fetchone() 
                 
-    # WICKET
-                for wicket in wickets:
-                    extras.execute_batch(cursor,cursor.mogrify("""
+    # WICKET    
+    wicketsValues= []    
+    for wicket in wickets:
+                    wicketsValues.append((delivery_id, wicket['player_out'], wicket['kind']))
+    extras.execute_batch(cursor,"""
                         INSERT INTO wickets (delivery_id, player_out_id, kind)
                         VALUES (%s, (SELECT id from players where name = %s limit 1), %s);
-                    """, (delivery_id, wicket['player_out'], wicket['kind'])),pageSize)
+                    """, wicketsValues,pageSize)
                 
     # EXTRAS
-                for extra_type, extra_count in extras.items():
-                    extras.execute_batch(cursor,cursor.mogrify("""
+    extrasValues= []
+    for extra_type, extra_count in extrass.items():
+                    extrasValues.append((delivery_id,extra_type, extra_count))
+    extras.execute_batch(cursor,"""
                         INSERT INTO extras (delivery_id, extra_type, count)
                         VALUES (%s, %s, %s);
-                    """, (delivery_id,extra_type, extra_count)),pageSize)
+                    """, extrasValues,pageSize)
